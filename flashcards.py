f5c6b6c3f1d444fa726b4d1315ad035a94924df6
BelalMou <bilalae2001@gmail.com>
2024-03-05 19:30:48 +0100

    adding time page for our flask app

--- flashcards.py
+++ flashcards.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def welcome():
     return "Welcome to my flash cards application!"
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
