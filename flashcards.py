617dfd062cd313b808735ee079657d8eb35ccadf
BelalMou <bilalae2001@gmail.com>
2024-03-06 18:29:23 +0100

    adding time page for our flask app

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from model import db, save_db
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,7 +18,26 @@ def card_view(index):
     except IndexError:
         abort(404)
 
+@app.route("/add_card", methods=["GET", "POST"])
+def add_cards():
+    if request.method == "POST":
+        card = {"question": request.form['question'],
+                "answer": request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for("card_view", index=len(db)-1))
+    else:
+        return render_template("add_card.html")
 
+@app.route("/remove_card/<int:index>", methods=["GET","POST"])
+def remove_card(index):
+    card = db[index]
+    if request.method == "POST":
+        del db[index]
+        save_db()
+        return redirect(url_for('welcome'))
+    else:
+        return render_template("remove_card.html", card=card)
 @app.route("/api/card/")
 def api_card_list():
     return jsonify(db)
